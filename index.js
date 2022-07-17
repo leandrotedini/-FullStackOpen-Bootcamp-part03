0d56b9ddc2a8d1c85b290120b0c671344729b47c
leandrotedini <leandro.tedini@gmail.com>
2022-07-17 17:03:57 -0300

    Part 03 - Exercise 3.10 and 3.11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('person', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url - :response-time ms :person'))
